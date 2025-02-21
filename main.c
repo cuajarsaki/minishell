b8a2d6551e6a9a96ad90c7f7eafec45e7d319b87
sakichung <puiting127@gmail.com>
2025-02-21 09:19:15 +0900

    fix: reset signal state before handling input

--- main.c
+++ main.c
@@ -120,6 +120,7 @@ void set_prompt(const char *prompt, t_env *env_list)
     term_clear_screen();
     while (running) {
         write(STDOUT_FILENO, prompt, ft_strlen(prompt));
+        g_signal_received=0;
         ft_memset(buf, 0, sizeof(buf));
         len = 0;
         handle_input(buf, &len, sizeof(buf));
