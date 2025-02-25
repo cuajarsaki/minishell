060ad61e871218e1f1ec9740069402def66cee00
sakichung <puiting127@gmail.com>
2025-02-25 23:52:04 +0900

    feat: implement readline for input handling and update history management

--- shell_termcaps/termcaps.c
+++ shell_termcaps/termcaps.c
@@ -64,77 +64,95 @@ void history_down(char *buf, size_t *len, size_t *cursor_pos) {
 }
 
 void handle_input(char *buf, size_t *len, size_t max_len) {
-    char c;
-    size_t cursor_pos = *len; // Tracks the current cursor position
-    ssize_t r ;
+    // char c;
+    // size_t cursor_pos = *len; // Tracks the current cursor position
+    // ssize_t r ;
+    (void) max_len;
 
-    while ((r=read(STDIN_FILENO, &c, 1)) >= 0)
+    char *str;
+    str = readline("minishell ❤ ");
+    if (str == NULL)
     {
-        if (c == '\033') { // Escape character
-            char seq[3] = {0};
-            if (read(STDIN_FILENO, &seq[0], 1) == 0) continue;
-            if (read(STDIN_FILENO, &seq[1], 1) == 0) continue;
-
-            if (seq[0] == '[') {
-                if (seq[1] == 'A') { // Up arrow
-                    history_up(buf, len, &cursor_pos);
-                } else if (seq[1] == 'B') { // Down arrow
-                    history_down(buf, len, &cursor_pos);
-                } else if (seq[1] == 'D') { // Left arrow
-                    if (cursor_pos > 0) {
-                        cursor_pos--;
-                        write(STDOUT_FILENO, "\033[D", 3); // Move cursor left
-                    }
-                } else if (seq[1] == 'C') { // Right arrow
-                    if (cursor_pos < *len) {
-                        cursor_pos++;
-                        write(STDOUT_FILENO, "\033[C", 3); // Move cursor right
-                    }
-                }
-            }
-            continue;
-        } else if (c == '\n') { // Enter key
-            buf[*len] = '\0';
-            write(STDOUT_FILENO, "\n", 1);
-            add_to_history(buf);
-            return;
-        } else if (c == 127) { // Backspace key
-            if (cursor_pos > 0) {
-                for (size_t i = cursor_pos - 1; i < *len; i++) {
-                    buf[i] = buf[i + 1];
-                }
-                (*len)--;
-                cursor_pos--;
-                write(STDOUT_FILENO, "\r\033[K", 4); // Clear the line
-                write(STDOUT_FILENO, "b1ffshell ❤  ", 14);
-                write(STDOUT_FILENO, buf, *len); // Rewrite the buffer
-                write(STDOUT_FILENO, "\033[D", 3 * (*len - cursor_pos)); // Move cursor back
-            }
-        }
-        else if (r == 0)
-        { // Ctrl-D
-            if (*len == 0) {
-                write(STDOUT_FILENO, "exit\n", 5);
-                exit(0);
-            }
-        }
-        else if (c == 31)
-        { // Ctrl-/
-            write(STDOUT_FILENO, "\r\033[K", 4);
-            *len = 0;
-            return;
-        }
-        else if (*len < max_len - 1)
-        {
-            for (size_t i = *len; i > cursor_pos; i--) {
-                buf[i] = buf[i - 1];
-            }
-            buf[cursor_pos++] = c;
-            (*len)++;
-            write(STDOUT_FILENO, "\r\033[K", 4); // Clear the line
-            write(STDOUT_FILENO, "b2ffshell ❤  ", 14);
-            write(STDOUT_FILENO, buf, *len); // Rewrite the buffer
-            write(STDOUT_FILENO, "\033[D", 3 * (*len - cursor_pos)); // Move cursor back
-        }
+        write(STDOUT_FILENO, "exit\n", 5);
+        exit(0);
     }
+    else if (ft_strcmp(str, "\n") == 0)
+    {
+        write(STDOUT_FILENO, "\n", 1);
+        return;
+    }
+    add_history(str);
+    strcpy(buf, str); // TODO: fix
+    *len = ft_strlen(buf;)
+    free(str);
+
+    // while ((r = read(STDIN_FILENO, &c, 1)) >= 0)
+    // {
+    //     if (c == '\033') { // Escape character
+    //         char seq[3] = {0};
+    //         if (read(STDIN_FILENO, &seq[0], 1) == 0) continue;
+    //         if (read(STDIN_FILENO, &seq[1], 1) == 0) continue;
+
+    //         if (seq[0] == '[') {
+    //             if (seq[1] == 'A') { // Up arrow
+    //                 history_up(buf, len, &cursor_pos);
+    //             } else if (seq[1] == 'B') { // Down arrow
+    //                 history_down(buf, len, &cursor_pos);
+    //             } else if (seq[1] == 'D') { // Left arrow
+    //                 if (cursor_pos > 0) {
+    //                     cursor_pos--;
+    //                     write(STDOUT_FILENO, "\033[D", 3); // Move cursor left
+    //                 }
+    //             } else if (seq[1] == 'C') { // Right arrow
+    //                 if (cursor_pos < *len) {
+    //                     cursor_pos++;
+    //                     write(STDOUT_FILENO, "\033[C", 3); // Move cursor right
+    //                 }
+    //             }
+    //         }
+    //         continue;
+    //     } else if (c == '\n') { // Enter key
+    //         buf[*len] = '\0';
+    //         write(STDOUT_FILENO, "\n", 1);
+    //         add_to_history(buf);
+    //         return;
+    //     } else if (c == 127) { // Backspace key
+    //         if (cursor_pos > 0) {
+    //             for (size_t i = cursor_pos - 1; i < *len; i++) {
+    //                 buf[i] = buf[i + 1];
+    //             }
+    //             (*len)--;
+    //             cursor_pos--;
+    //             write(STDOUT_FILENO, "\r\033[K", 4); // Clear the line
+    //             write(STDOUT_FILENO, "b1ffshell ❤  ", 14);
+    //             write(STDOUT_FILENO, buf, *len); // Rewrite the buffer
+    //             write(STDOUT_FILENO, "\033[D", 3 * (*len - cursor_pos)); // Move cursor back
+    //         }
+    //     }
+    //     else if (r == 0)
+    //     { // Ctrl-D
+    //         if (*len == 0) {
+    //             write(STDOUT_FILENO, "exit\n", 5);
+    //             exit(0);
+    //         }
+    //     }
+    //     else if (c == 31)
+    //     { // Ctrl-/
+    //         write(STDOUT_FILENO, "\r\033[K", 4);
+    //         *len = 0;
+    //         return;
+    //     }
+    //     else if (*len < max_len - 1)
+    //     {
+    //         for (size_t i = *len; i > cursor_pos; i--) {
+    //             buf[i] = buf[i - 1];
+    //         }
+    //         buf[cursor_pos++] = c;
+    //         (*len)++;
+    //         write(STDOUT_FILENO, "\r\033[K", 4); // Clear the line
+    //         write(STDOUT_FILENO, "b2ffshell ❤  ", 14);
+    //         write(STDOUT_FILENO, buf, *len); // Rewrite the buffer
+    //         write(STDOUT_FILENO, "\033[D", 3 * (*len - cursor_pos)); // Move cursor back
+    //     }
+    // }
 }
