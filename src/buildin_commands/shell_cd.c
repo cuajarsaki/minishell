eb67a3d70752b1b14ba6a30641f9d4946819a090
sakichung <puiting127@gmail.com>
2025-03-04 01:07:00 +0900

    fix: update shell_cd function to return exit status

--- src/buildin_commands/shell_cd.c
+++ src/buildin_commands/shell_cd.c
@@ -6,19 +6,19 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 15:41:50 by jidler            #+#    #+#             */
-/*   Updated: 2025/02/28 12:06:18 by pchung           ###   ########.fr       */
+/*   Updated: 2025/03/04 00:49:51 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../shell.h"
 
-void shell_cd(char *path)
+int shell_cd(char *path)
 {
 	if (path == NULL || ft_strcmp(path, "") == 0) {
 		char *home = getenv("HOME");
 		if (home == NULL) {
 			write(2, "cd: HOME not set\n", 17);
-			return;
+			return 1;
 		}
 		path = home;
 	}
@@ -32,6 +32,8 @@ void shell_cd(char *path)
 			write(1, "\n", 1);
 		} else {
 			perror("getcwd");
+			return 1;
 		}
 	}
+	return 0;
 }
