7bad740b76f3d0d7660a5a5be24006af6dfb94d0
sakichung <puiting127@gmail.com>
2025-03-04 01:07:16 +0900

    fix: update shell_env function to return exit status and handle empty environment list

--- src/buildin_commands/shell_env.c
+++ src/buildin_commands/shell_env.c
@@ -1,10 +1,17 @@
 #include "../../shell.h"
 
-void shell_env(t_env *env_list)
+int shell_env(t_env *env_list)
 {
+    if (!env_list)
+    {
+        printf("No environment variables to display.\n");
+        return 1;
+    }
+
     while (env_list)
     {
         printf("%s=%s\n", env_list->key, env_list->value);
         env_list = env_list->next;
     }
+    return 0;
 }
