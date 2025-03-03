146e277488a39f39e30aac975cb0a7d999c9a506
sakichung <puiting127@gmail.com>
2025-03-04 01:07:06 +0900

    fix: change shell_echo function to return an integer exit status

--- src/buildin_commands/shell_echo.c
+++ src/buildin_commands/shell_echo.c
@@ -6,16 +6,16 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 15:41:50 by jidler            #+#    #+#             */
-/*   Updated: 2025/02/28 12:06:08 by pchung           ###   ########.fr       */
+/*   Updated: 2025/03/04 00:55:46 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../shell.h"
 
-void shell_echo(char **args)
+int  shell_echo(char **args)
 {
 	// compile error occurs if dont comment out this line?
-	// need to be double check Å´
+	// need to be double check ÔøΩÔøΩ
 	// bool newline = true;
 	int i = 0;
 	
@@ -33,4 +33,6 @@ void shell_echo(char **args)
 	}
 	if (newline)
 		write(1, "\n", 1);
+	
+	return 0;
 }
