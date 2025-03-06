f16ca84b8310c80b985db8a6c3c5a1e2a0e03410
sakichung <puiting127@gmail.com>
2025-03-06 21:47:17 +0900

    refactor: fix norminette in signal.c and signal_2.c

--- src/signal/signal.c
+++ src/signal/signal.c
@@ -6,31 +6,28 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 19:57:16 by pchung            #+#    #+#             */
-/*   Updated: 2025/03/05 21:17:43 by pchung           ###   ########.fr       */
+/*   Updated: 2025/03/06 21:46:55 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../shell.h"
 
-volatile sig_atomic_t g_signal_received = 0;
+volatile sig_atomic_t	g_signal_received = 0;
 
-static int event_hook_readline(void);
-
-void init_readline_for_signal(void)
+static int	event_hook_readline(void)
 {
-    rl_outstream = stderr;
-    rl_done = 0;
-    rl_catch_signals = 0;
-    rl_event_hook = event_hook_readline;
+	if (g_signal_received == SIGINT)
+	{
+		rl_replace_line("", 0);
+		rl_done = 1;
+	}
+	return (0);
 }
 
-static int event_hook_readline(void)
+void	init_readline_for_signal(void)
 {
-    if (g_signal_received == SIGINT)
-    {
-        rl_replace_line("", 0);
-        rl_done = 1;
-    }
-    return (0);
+	rl_outstream = stderr;
+	rl_done = 0;
+	rl_catch_signals = 0;
+	rl_event_hook = event_hook_readline;
 }
-
