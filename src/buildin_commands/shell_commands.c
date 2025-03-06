4202eeaa0280273bf92505c72b06ad0a591078ed
cuajarsaki <puiting127@gmail.com>
2025-03-06 12:53:00 +0900

    refactor: fix norminette in shell_commands.c

--- src/buildin_commands/shell_commands.c
+++ src/buildin_commands/shell_commands.c
@@ -12,13 +12,14 @@
 
 #include "../../shell.h"
 
-void shell_unknown_command(char *cmd) {
+void	shell_unknown_command(char *cmd)
+{
 	write(2, "unknown command: ", 18);
 	write(2, cmd, ft_strlen(cmd));
 	write(2, "\n", 1);
 }
 
-void shell_clear(void)
+void	shell_clear(void)
 {
 	write(STDOUT_FILENO, "\033[2J\033[H", 7);
 }
