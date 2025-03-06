3e37f8d35eab8908f9469b9740f82569baa61541
sakichung <puiting127@gmail.com>
2025-03-06 21:57:21 +0900

    refactor: fix norminette in cmd_tokenizer.c

--- src/cmd_tokenizer/cmd_tokenizer.c
+++ src/cmd_tokenizer/cmd_tokenizer.c
@@ -3,26 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_tokenizer.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jidler <jidler@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 18:07:42 by pchung            #+#    #+#             */
-/*   Updated: 2025/03/04 16:13:20 by jidler           ###   ########.fr       */
+/*   Updated: 2025/03/06 21:56:49 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../shell.h"
 
-
-static size_t argv_len(t_cmd *cmd)
+static size_t	argv_len(t_cmd *cmd)
 {
-    size_t len = 0;
-    t_list *tmp = cmd->tokens; 
-    while (tmp != NULL)
-    {
-        len++;
-        tmp = tmp->next;
-    }
-    return len;
+	size_t	len;
+	t_list	*tmp;
+
+	len = 0;
+	tmp = cmd->tokens;
+	while (tmp != NULL)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+	return (len);
 }
 
 static char	*xstrdup(const char *s1)
@@ -35,29 +37,25 @@ static char	*xstrdup(const char *s1)
 	return (tmp);
 }
 
-char **token_list_to_argv(t_cmd *cmd)
+char	**token_list_to_argv(t_cmd *cmd)
 {
-    char **argv;
-    size_t i;
-    size_t len;
-    t_list *current;
+	char	**argv;
+	size_t	i;
+	size_t	len;
+	t_list	*current;
 
-    len = argv_len(cmd);
-
-    argv = (char **)malloc((len + 1) * sizeof(char *));
-    if (argv == NULL)
-        perror("token_list_to_argv");
-
-    // Populate the array with the contents of the linked list
-    current = cmd->tokens;
-    for (i = 0; i < len; i++)
-    {
-        argv[i] = xstrdup((char *)current->content);
-        current = current->next;
-    }
-    
-    // Null-terminate the array
-    argv[len] = NULL;
-
-    return argv;
+	len = argv_len(cmd);
+	argv = (char **)malloc((len + 1) * sizeof(char *));
+	if (argv == NULL)
+		perror("token_list_to_argv");
+	current = cmd->tokens;
+	i = 0;
+	while (i < len)
+	{
+		argv[i] = xstrdup((char *)current->content);
+		current = current->next;
+		i++;
+	}
+	argv[len] = NULL;
+	return (argv);
 }
