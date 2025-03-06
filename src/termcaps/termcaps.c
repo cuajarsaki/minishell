54ae13b567591acc61ad8c3b60d1e9cb040dce57
sakichung <puiting127@gmail.com>
2025-03-06 21:50:15 +0900

    refactor: fix norminette in termcaps.c

--- src/termcaps/termcaps.c
+++ src/termcaps/termcaps.c
@@ -6,31 +6,30 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 00:08:24 by pchung            #+#    #+#             */
-/*   Updated: 2025/03/05 23:07:53 by pchung           ###   ########.fr       */
+/*   Updated: 2025/03/06 21:50:06 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../shell.h"
 
-
-void	exit_program()
+void	exit_program(void)
 {
 	exit(0);
 }
 
+void	handle_input(char *buf, size_t *len, size_t buffsize)
+{
+	char	*str;
 
-void handle_input(char *buf, size_t *len, size_t buffsize) {
-    g_signal_received = NOTSIG;
-    char *str;   
-    str = readline("minishell ❤ ");
-    if (str == NULL)
-    {
-        write(STDOUT_FILENO, "exit\n", 5);
-        exit(0);
-    }
-
-    add_history(str);
-    ft_strlcpy(buf, str, buffsize); 
-    *len = ft_strlen(buf);
-    free(str);
+	g_signal_received = NOTSIG;
+	str = readline("minishell ❤ ");
+	if (str == NULL)
+	{
+		write(STDOUT_FILENO, "exit\n", 5);
+		exit(0);
+	}
+	add_history(str);
+	ft_strlcpy(buf, str, buffsize);
+	*len = ft_strlen(buf);
+	free(str);
 }
