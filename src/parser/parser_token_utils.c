6eb2fca534f23300573f5ed709d8a1fbd3b799fe
cuajarsaki <puiting127@gmail.com>
2025-03-07 09:32:52 +0900

    refactor: fix norminette in parser_ast.c, parser_redir.c, parser_token_utils.c, and parser_utils.c

--- src/parser/parser_token_utils.c
+++ src/parser/parser_token_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_token_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 09:09:33 by pchung            #+#    #+#             */
+/*   Updated: 2025/03/07 09:09:45 by pchung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parser.h"
 
 char	*remove_quotes(const char *token)
@@ -7,18 +19,14 @@ char	*remove_quotes(const char *token)
 
 	if (!token)
 		return (NULL);
-
 	len = ft_strlen(token);
 	if (len < 2)
-		return ft_strdup(token);
-
+		return (ft_strdup(token));
 	if ((token[0] == '\'' || token[0] == '\"') && token[0] == token[len - 1])
 	{
-
 		new_token = ft_strndup(token + 1, len - 2);
 	}
 	else
 		new_token = ft_strdup(token);
-
-	return new_token;
+	return (new_token);
 }
